684754957d91b8f236942098fc7efa02695a2dd1
Rui Paulo Calei <ruipaulocalei0@gmail.com>
2021-12-31 12:05:34 +0100

    Users Service

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -1,6 +1,5 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from "@nestjs/common";
-// import { PrismaClient } from "../generated/client";
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "../generated/client";
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,6 +1,7 @@
 import { Query, Resolver } from "@nestjs/graphql";
+import { UserModel } from "src/models/users.model";
 
-@Resolver()
+@Resolver(of => UserModel)
 export class UsersResolver {
   @Query(() => Boolean)
   isGood(): boolean {
