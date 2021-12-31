71deaa7509a8cac1e853524eea12329598bf0e77
Rui Paulo Calei <ruipaulocalei0@gmail.com>
2021-12-31 19:49:20 +0100

    CartItem Model

---
+++ src/models/cart-items.model.ts
@@ -0,0 +1,25 @@
+import { Field, InputType, ObjectType } from "@nestjs/graphql";
+import { IsString, IsNumber, Length } from 'class-validator'
+import { ProductModel } from "./products.model";
+import { UserModel } from "./users.model";
+
+@InputType('CartItemModelPrisma', { isAbstract: true })
+@ObjectType()
+export class CartItemModel {
+  @Field(type => String)
+  id: string
+  @Field(type => Number, { defaultValue: 1 })
+  @IsNumber()
+  quantity: number
+
+  @Field(type => [UserModel])
+  user: UserModel[]
+
+  @Field(type => [ProductModel])
+  product: ProductModel[]
+
+  @Field(type => Date)
+  createdAt: Date
+  @Field(type => Date)
+  updatedAt: Date
+}
